6955b3bc8c3d4fe4492eb5824d8126fc2d3d4c9e
Luis Montes <luis@iceddev.com>
2014-08-19 19:00:17 -0700

    support for messaging

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict';
 
 var alljoyn = require('alljoyn');
+var _ = require('lodash');
 
 function Plugin(messenger, options){
   this.messenger = messenger;
   this.options = options;
+  var self = this;
 
-  var sessions = this.sessions = [];
+  self.sessions = [];
 
   function onFound(name){
     console.log('FoundAdvertisedName', name);
     var sessionId = bus.joinSession(name, 27, 0);
     console.log('JoinSession ' + sessionId);
-    sessions.push(sessionId);
+    self.sessions = _.union([sessionId], self.sessions);
   }
 
   function onLost(name){
@@ -24,33 +26,83 @@ function Plugin(messenger, options){
   }
 
   var bus = this.bus = alljoyn.BusAttachment('skynet-alljoyn');
-  var inter = alljoyn.InterfaceDescription();
+  var inter = this.inter = alljoyn.InterfaceDescription();
   var listener = alljoyn.BusListener(onFound, onLost, onChanged);
 
-  bus.createInterface(this.options.advertisedName, inter);
+  bus.createInterface(this.options.interfaceName, inter);
   bus.registerBusListener(listener);
 
+  function onAcceptSessionJoiner(port, joiner){
+    console.log("AcceptSessionJoiner", port, joiner);
+    //TODO possibly be more selective
+    return true;
+  }
+
+  function onSessionJoined(port, sId, joiner){
+    self.sessions = _.union([sId], self.sessions);
+    console.log("SessionJoined", port, sId, joiner);
+  }
+
+  if(this.options.advertisedName){
+
+    var portListener = alljoyn.SessionPortListener(onAcceptSessionJoiner, onSessionJoined);
+    var fullName = this.options.interfaceName + '.' + this.options.advertisedName;
+    console.log("RequestName " + bus.requestName(fullName));
+    console.log("AdvertiseName " + bus.advertiseName(fullName));
+    console.log("BindSessionPort " + bus.bindSessionPort(27, portListener));
+  }
+
   bus.start();
+
+  inter.addSignal(this.options.signalMemberName, "s", "msg");
+  var messageObject = this.messageObject = alljoyn.BusObject(this.options.messageServiceName);
+  messageObject.addInterface(inter);
+
+  function onSignalReceived(msg, info){
+    console.log("Signal received: ", msg, info);
+    messenger.send({
+      devices: self.options.relayUuid,
+      payload: {
+        msg: msg,
+        info: info
+      }
+    });
+  }
+  if(this.options.relayUuid){
+    bus.registerSignalHandler(messageObject, onSignalReceived, inter, this.options.signalMemberName);
+  }
+
+  bus.registerBusObject(messageObject);
+
   bus.connect();
+
+  if(this.options.findAdvertisedName){
+    bus.findAdvertisedName(this.options.findAdvertisedName);
+  }
+
 
   return this;
 }
 
 Plugin.prototype.onMessage = function(data, callback){
   console.log('skynet-alljoyn message received: ', data);
+  var self = this;
 
-  if(data.payload.method === 'send'){
-    this.bus.findAdvertisedName(this.options.advertisedName);
+  if(!data.payload){ return; }
 
-    // TODO: send message
+  if(data.payload.method === 'send' && data.payload.message){
+    self.sessions.forEach(function(sessionId){
+      console.log('sending from skynet to session', sessionId, self.options.signalMemberName);
+      self.messageObject.signal(null, sessionId, self.inter, self.options.signalMemberName, data.payload.message);
+    });
   }
 
-  if(data.payload.method === 'listen'){
-    // TODO: listen for messages
+  if(data.payload.method === 'notify'){
+    // TODO: send notification
   }
 
   if(callback){
-    callback(data);
+    callback(data.payload);
   }
 };
 
@@ -77,13 +129,46 @@ var optionsSchema = {
   properties: {
     advertisedName: {
       type: 'string',
+      required: false
+    },
+    interfaceName: {
+      type: 'string',
       required: true
+    },
+    findAdvertisedName: {
+      type: 'string',
+      required: false
+    },
+    signalMemberName: {
+      type: 'string',
+      required: true
+    },
+    messageServiceName: {
+      type: 'string',
+      required: true
+    },
+    relayUuid: {
+      type: 'string',
+      required: false
     }
+
   }
 };
+
+function getDefaultOptions(callback){
+    callback(null, {
+      advertisedName: 'test',
+      interfaceName: 'org.alljoyn.bus.samples.chat',
+      findAdvertisedName: 'org.alljoyn.bus.samples.chat',
+      signalMemberName: 'Chat',
+      messageServiceName: '/chatService',
+      relayUuid: '*'
+    });
+}
 
 module.exports = {
   Plugin: Plugin,
   messageSchema: messageSchema,
-  optionsSchema: optionsSchema
+  optionsSchema: optionsSchema,
+  getDefaultOptions: getDefaultOptions
 };
